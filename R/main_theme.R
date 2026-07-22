865a4721df177b27d221b0ea2adb2faa671ac3cd
gage1145 <140093724+gage1145@users.noreply.github.com>
2026-07-22 16:51:32 -0500

    Updated figures.

--- R/main_theme.R
+++ R/main_theme.R
@@ -5,6 +5,6 @@ main_theme <- theme(
   legend.title = element_text(size = 24),
   legend.text = element_text(size = 20),
   strip.text = element_text(size = 24),
-  panel.background = element_rect(fill = "white"),
-  panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
+  # panel.background = element_rect(fill = "white"),
+  # panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
 )
